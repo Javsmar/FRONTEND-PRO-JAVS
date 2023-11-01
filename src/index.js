dab9059a5f010b07cd97c5352159c5ed89254b83
Javier <mastos44@gmail.com>
2023-11-01 12:41:59 +0100

    Webpack1: initial config

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//import _ from 'lodash'; importa todas las librerias de lodash
+
+import join from 'lodash/join';
+
+const hello = 'Hola';
+const world = ' mundo';
+
+console.log(join([hello, world], ' '));
+
+
+console.log(`${hello} ${world}`);
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        app: './src/app.js'
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    output: {
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist',
+        clean: true
+    }
+}
